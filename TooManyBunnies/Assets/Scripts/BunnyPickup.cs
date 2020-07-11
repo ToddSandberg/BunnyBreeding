4b20c5968bb8e35aa91dee9687a32aa4c69cb8de
ToddSandberg <sandbergs.todd@gmail.com>
2020-07-11 00:22:07 -0700

    hold the bumny

--- TooManyBunnies/Assets/Scripts/BunnyPickup.cs
+++ TooManyBunnies/Assets/Scripts/BunnyPickup.cs
@@ -6,15 +6,23 @@ public class BunnyPickup : MonoBehaviour
 {
     public GameObject player;
     public GameObject playerHand;
+    private bool isBeingHeld;
 
     void Update()
     {
         if (Input.GetKeyDown (KeyCode.E)) {
-			if (this.GetComponent<CircleCollider2D> ().IsTouching (player.GetComponent<CircleCollider2D>())) {
-				transform.SetParent(playerHand.transform);
-                transform.localPosition = new Vector3(0, 0, -1);
-                this.GetComponent<BunnyAI>().bunnySpeed = 0;
-			}
+            if (isBeingHeld) {
+                transform.parent = null;
+                this.GetComponent<BunnyAI>().enabled = true;
+                isBeingHeld = false;
+            } else {
+                if (this.GetComponent<CircleCollider2D> ().IsTouching (player.GetComponent<CircleCollider2D>())) {
+                    transform.SetParent(playerHand.transform);
+                    transform.localPosition = new Vector3(0, 0, -1);
+                    this.GetComponent<BunnyAI>().enabled = false;
+                    isBeingHeld = true;
+                }
+            }
 		}
     }
 }
