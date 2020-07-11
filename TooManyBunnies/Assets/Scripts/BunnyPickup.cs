49e85d5bdbe62d68bc5f90becfceb0a4f756c169
ToddSandberg <sandbergs.todd@gmail.com>
2020-07-11 00:12:16 -0700

    hey hey hey heres some changes yo

---
+++ TooManyBunnies/Assets/Scripts/BunnyPickup.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BunnyPickup : MonoBehaviour
+{
+    public GameObject player;
+    public GameObject playerHand;
+
+    void Update()
+    {
+        if (Input.GetKeyDown (KeyCode.E)) {
+			if (this.GetComponent<CircleCollider2D> ().IsTouching (player.GetComponent<CircleCollider2D>())) {
+				transform.SetParent(playerHand.transform);
+                transform.localPosition = new Vector3(0, 0, -1);
+                this.GetComponent<BunnyAI>().bunnySpeed = 0;
+			}
+		}
+    }
+}
