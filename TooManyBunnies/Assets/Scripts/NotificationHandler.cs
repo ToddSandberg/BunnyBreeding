9a986081e033dd99321f2e07fe2377f515f99a18
ToddSandberg <sandbergs.todd@gmail.com>
2020-07-12 11:20:58 -0700

    Fix some warnings

--- TooManyBunnies/Assets/Scripts/NotificationHandler.cs
+++ TooManyBunnies/Assets/Scripts/NotificationHandler.cs
@@ -9,7 +9,7 @@ public class NotificationHandler : MonoBehaviour
     public void createNotification(string text, Vector3 worldPosition) {
         GameObject notif = Instantiate(notification);
         notif.GetComponentsInChildren<Text>()[0].text = text;
-        notif.transform.parent = gameObject.transform;
+        notif.transform.SetParent(gameObject.transform);
         Vector3 screenPos = Camera.main.WorldToScreenPoint(worldPosition);
         notif.GetComponent<RectTransform>().anchoredPosition = new Vector3(screenPos.x - 100, screenPos.y + 50, 0);
     }
